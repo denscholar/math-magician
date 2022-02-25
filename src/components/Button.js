5d3bdafa77eecaf69633abd98fcbfe78fe26e3c0
Dennis Akagha <d.akagha20@gmail.com>
2022-02-25 17:29:33 +0100

    Refactor the calculator to functional component with hooks

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/prop-types */
-
 import React from 'react';
 
 class Button extends React.Component {
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,66 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import Button from './Button';
 import calculate from '../logic/Calculate';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: {},
-    };
-    this.handleClick = this.handleClick.bind(this);
+const Calculator = () => {
+  let output = 0;
+  const [calculateValaue, setCalculateValaue] = useState({});
+
+  const handleClick = (obj, value) => {
+    setCalculateValaue(calculate(obj, value));
+  };
+
+  if (Object.keys(calculateValaue).length === 0 || (calculateValaue.total === null
+    && calculateValaue.next === null
+    && calculateValaue.operation === null)) {
+    output = 0;
+  } else {
+    output = calculateValaue.next !== null ? calculateValaue.next : calculateValaue.total;
   }
 
-  handleClick(obj, value) {
-    this.setState({ result: calculate(obj, value) });
-  }
+  return (
+    <div className="container">
+      <div className="calculator">
+        <div className="result">
+          { output }
+        </div>
+        <div className="calculator-buttons">
+          <Button value="AC" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="+/-" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="%" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="&divide;" className="btn orange" onClick={handleClick} calcObject={calculateValaue} />
 
-  render() {
-    const { result } = this.state;
-    let output = 0;
-    if (Object.keys(result).length === 0 || (result.total === null
-      && result.next === null
-      && result.operation === null)) {
-      output = 0;
-    } else {
-      output = result.next !== null ? result.next : result.total;
-    }
-    return (
-      <div className="container">
-        <div className="calculator">
-          <div className="result">
-            { output }
-          </div>
-          <div className="calculator-buttons">
-            <Button value="AC" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="+/-" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="%" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="&divide;" className="btn orange" onClick={this.handleClick} calcObject={result} />
+          <Button value="7" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="8" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="9" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="x" className="btn orange" onClick={handleClick} calcObject={calculateValaue} />
 
-            <Button value="7" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="8" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="9" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="x" className="btn orange" onClick={this.handleClick} calcObject={result} />
+          <Button value="4" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="5" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="6" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="-" className="btn orange" onClick={handleClick} calcObject={calculateValaue} />
 
-            <Button value="4" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="5" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="6" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="-" className="btn orange" onClick={this.handleClick} calcObject={result} />
+          <Button value="1" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="2" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="3" className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="+" className="btn orange" onClick={handleClick} calcObject={calculateValaue} />
 
-            <Button value="1" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="2" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="3" className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="+" className="btn orange" onClick={this.handleClick} calcObject={result} />
-
-            <Button value="0" className="btn span-3" onClick={this.handleClick} calcObject={result} />
-            <Button value="." className="btn" onClick={this.handleClick} calcObject={result} />
-            <Button value="=" className="btn orange" onClick={this.handleClick} calcObject={result} />
-          </div>
+          <Button value="0" className="btn span-3" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="." className="btn" onClick={handleClick} calcObject={calculateValaue} />
+          <Button value="=" className="btn orange" onClick={handleClick} calcObject={calculateValaue} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
