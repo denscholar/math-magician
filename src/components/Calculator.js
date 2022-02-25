071220b9e07cbf4a88b66c1f0fee602dcf65a98a
Dennis Akagha <d.akagha20@gmail.com>
2022-02-25 17:41:24 +0100

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import './Calculator.css';
 import Button from './Button';
+import './Calculator.css';
 import calculate from '../logic/Calculate';
 
 const Calculator = () => {
